bb861ab487524af3378249fb4e767d90b9569c28
Luiz Oliveira <luizcdev@gmail.com>
2019-10-23 23:28:13 -0300

    Criando banco de dados

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '895662057f469cd41b55621f80f6048a',
+  expiresIn: '7d',
+};
--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: 'gympass',
-  database: 'gympass',
+  password: 'docker',
+  database: 'gympoint',
   define: {
     timestamps: true,
     underscored: true,
